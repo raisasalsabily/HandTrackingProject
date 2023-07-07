369c41d012bfd234bde6e56a2f45d0720c048a1a
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2023-07-07 19:34:08 +0700

    fix volumehandcontrol

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -4,9 +4,10 @@ import numpy as np
 import math
 import HandTrackingModule as htm
 
+from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-
+ 
 ################################
 wCam, hCam = 640, 480
 ################################
@@ -22,14 +23,17 @@ detector = htm.handDetector(detectionCon=0.7)
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
     IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = interface.QueryInterface(IAudioEndpointVolume)
+# volume = interface.QueryInterface(IAudioEndpointVolume)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
 volRange = volume.GetVolumeRange()
 # volume.SetMasterVolumeLevel(0, None)
 minVol = volRange[0]
 maxVol = volRange[1]
-
+vol = 0
+volBar = 180
+volPer = 0
 
 while True:
     # Membaca setiap img (frame) dari webcam
@@ -54,17 +58,22 @@ while True:
         length = math.hypot(x2-x1, y2-y1)
         # print(length) 
 
-        # hand range 50 - 300
+        # hand range 50 - 180
         # volume range: -45 - 0
         # convert hand range to volume range
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
+        vol = np.interp(length, [50, 180], [minVol, maxVol])
+        volBar = np.interp(length, [50, 180], [400, 150])
+        volPer = np.interp(length, [50, 180], [0, 100])
         print(int(length), vol)
-        volume.SetMasterVolumeLevel(0, None)
+        volume.SetMasterVolumeLevel(vol, None)
 
         # change color to green if length < 50
         if length<50:
-            cv2.circle(img, (cx,cy), 11, (0,255,0), cv2.FILLED)
+            cv2.circle(img, (cx,cy), 11, (0,255,0), cv2.FILLED) 
 
+    cv2.rectangle(img, (50,180), (85,180), (255,0,0), 3)
+    cv2.rectangle(img, (50,int(volBar)), (85,180), (255,0,0), cv2.FILLED)
+    cv2.putText(img, f': {int(volPer)} %', (40,450), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 3)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
